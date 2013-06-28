15cab7ce6cc60746a0012cf1cf090c1c5be342b0
Yunlong Mao <b43258@freescale.com>
2013-06-28 16:01:51 +0800

    Initial project

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<script src="<?php echo get_template_directory_uri(); ?>/static/js/jquery-1.8.2.min.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/static/js/bootstrap.min.js" type="text/javascript"></script>
+<?php
+    wp_head();
+?>
+</head>
+
+<body <?php body_class(); ?>>
+<div class="container">
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="row-fluid">
+    <div class="span2">
+    	 span2
+    </div>
+    <div class="span10">
+      	 span10
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
